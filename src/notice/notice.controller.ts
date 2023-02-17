78d2a7a8b829c72b55bf8ba7e72060e3e13d2a1b
cocoa-dev-1 <sinmounbu0510@naver.com>
2023-02-18 01:57:52 +0900

    docs: add notice api to swagger docs

--- src/notice/notice.controller.ts
+++ src/notice/notice.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get } from '@nestjs/common';
 import { NoticeService } from './notice.service';
 import { NoticeEntity } from '../entitys/main/notice.entity';
 import { CategoriesService } from '../categories/categories.service';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('notice')
 @Controller('notice')
 export class NoticeController {
   constructor(
@@ -10,16 +12,42 @@ export class NoticeController {
     private readonly categoriesService: CategoriesService,
   ) {}
 
+  @ApiOperation({
+    summary: '모든 공지 가져오기',
+    description: '모든 공지를 가져옵니다.',
+  })
+  @ApiOkResponse({
+    description: '모든 공지',
+    type: () => NoticeEntity,
+    isArray: true,
+  })
   @Get()
   async findAll(): Promise<Array<NoticeEntity>> {
     return await this.noticeService.findAll();
   }
 
+  @ApiOperation({
+    summary: '최근 공지 가져오기',
+    description: '최근 공지를 가져옵니다.',
+  })
+  @ApiOkResponse({
+    description: '최근 공지',
+    type: () => NoticeEntity,
+  })
   @Get('/latest')
   async findLatest(): Promise<NoticeEntity> {
     return await this.noticeService.findLatest();
   }
 
+  @ApiOperation({
+    summary: '공지 카테고리 가져오기',
+    description: '모든 공지 카테고리를 가져옵니다.',
+  })
+  @ApiOkResponse({
+    description: '카테고리들',
+    type: 'string',
+    isArray: true,
+  })
   @Get('/categories')
   async getAllCategories(): Promise<Array<string>> {
     const categories = await this.categoriesService.findCategoriesByType(
